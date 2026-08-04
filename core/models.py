7b65bc18b08d2669b8d6f027d6b4ae73994dd6a8
Aasish <gk.aasishkarki@gmail.com>
2026-08-04 13:34:47 +0545

    Made basic Movie & Genre models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100,unique=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    genre = models.ForeignKey(Genre,on_delete=models.CASCADE,related_name="movies")
+    released_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
