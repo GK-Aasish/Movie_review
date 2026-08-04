3bbc3626a975f7c56f45799d08bb129de78f2fb9
Aasish <gk.aasishkarki@gmail.com>
2026-08-04 12:32:28 +0545

    Basic home and register template

---
+++ core/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from core.views import home_view,register_view
+
+urlpatterns = [
+    path("",home_view,name="home"),
+    path("/register",register_view,name="register"),
+]
