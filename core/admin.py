b511085004ed3689e6701fdb70ca2d055585835f
Aasish <gk.aasishkarki@gmail.com>
2026-08-06 00:31:34 +0545

    Basic Review model

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from core.models import Movie,Genre
+from core.models import Movie,Genre,Reviews
 
 # Register your models here.
 admin.site.register(Genre)
 admin.site.register(Movie)
+admin.site.register(Reviews)
--- core/models.py
+++ core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 # Create your models here.
 class Genre(models.Model):
@@ -16,4 +18,18 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Reviews(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie,on_delete=models.CASCADE,related_name="reviews")
+    rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+
+    class Meta:
+        verbose_name = "Review"
+        verbose_name_plural = "Reviews"
+        unique_together = ('movie','user')
+
+    def __str__(self):
+        return str(self.user.username + "'s" + "Review of " + self.movie.title)
     
